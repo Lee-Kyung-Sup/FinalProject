6c9b29ecf4d617e616e57bc99d0635907b131977
wrwrq <154485017+wrwrq@users.noreply.github.com>
2024-03-08 18:07:50 +0900

    Fix - error

--- Assets/RawData/ItemDbSheet.cs
+++ Assets/RawData/ItemDbSheet.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-[ExcelAsset(AssetPath = "Resources/DB", ExcelName = "ItemDbSheet")]
+//[ExcelAsset(AssetPath = "Resources/DB", ExcelName = "ItemDbSheet")]
 public class ItemDbSheet : ScriptableObject
 {
 	public List<ItemData> Entities; // Replace 'EntityType' to an actual type that is serializable.
