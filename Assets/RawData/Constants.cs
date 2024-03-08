90eddfccd693cdd693c23d35546a0ff08891f52d
todreami <140941478+todreami@users.noreply.github.com>
2024-03-08 20:33:48 +0900

    test
    test

--- Assets/RawData/Constants.cs
+++ Assets/RawData/Constants.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Constants : MonoBehaviour
+namespace Constants
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum ItemType
     {
-        
+        Weapon,
+        Armor,
+        Consume
     }
 
-    // Update is called once per frame
-    void Update()
+    public enum ItemGrade
     {
-        
+        Normal,
+        Magic,
+        Rare,
+        Unique
+    }
+
+    public class Colors
+    {
+        public static readonly Color[] ItemGrade = new[]
+        {
+            new Color(0.7f, 0.68f, 0.68f),
+            new Color(0.234f, 0.56f, 0.95f),
+            new Color(0.95f, 0.67f, 0.24f),
+            new Color(1, 0.22f, 0.96f)
+        };
     }
 }
