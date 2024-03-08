dafc2484cd9aa64c745ad5239d40a1ccb1ea215b
todreami <140941478+todreami@users.noreply.github.com>
2024-03-08 16:57:43 +0900

    fix/jaehyun/edit excel file
    edit excel file(imcompleted database)

--- Assets/RawData/ItemDbSheet.cs
+++ Assets/RawData/ItemDbSheet.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-[ExcelAsset]
+[ExcelAsset(AssetPath = "Resources/DB", ExcelName = "ItemDbSheet")]
 public class ItemDbSheet : ScriptableObject
 {
 	public List<ItemData> Entities; // Replace 'EntityType' to an actual type that is serializable.
