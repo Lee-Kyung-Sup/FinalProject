74640d1b977cf49b1072854dbee5abe4ea0c1de2
todreami <140941478+todreami@users.noreply.github.com>
2024-03-11 20:47:51 +0900

    fix/jaehyun/add excel file and edit gamemanager
    add excel file and gamemanager(incompleted)

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue
+{
+    [Tooltip("대사 치는 캐릭터 이름")]
+    public string name;
+}
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,18 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameManager _instance; //½Ì±ÛÅæ
+
+    public Transform Player {  get; private set; }
+    [SerializeField] private string playerTag = "Player";
+    //private HealthSystem playerHealthSystem; //½½¶óÀÌ´õ Àû¿ëÄÚµå
+
+    [SerializeField] private TextMeshProUGUI waveText;
+    [SerializeField] private SliderJoint2D hpGaugeSlider;
+
+    public static GameManager Instance
     {
-        
+        get {
+
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+
+                if(_instance == null)
+                {
+                    GameObject gameManagerObject = new GameObject("GameManager");
+                    _instance = gameManagerObject.AddComponent<GameManager>();
+                }
+
+            }
+            return _instance; }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (_instance == null)
+        {
+            _instance = this;
+            Player = GameObject.FindGameObjectWithTag(playerTag).transform;
+
+            //playerHealthSystem = Player.GetComponent<HealthSystem>();
+            //playerHealthSystem.OnDamage += UpdateHealthUI;
+            //playerHealthSystem.OnHeal += UpdateHealthUI;
+            //playerHealthSystem.OnDeath += GameOver;
+
+            //gameOverUI.SetActive(false);
+
+            //DontDestroyOnLoad(gameObject); //¾À ÀüÈ¯½Ã ÆÄ±«¹æÁö
+        }
+        else
+        {
+            //Destroy(gameObject);
+        }
     }
+
+    private void UpdateHealthUI()
+    {
+        //hpGaugeSlider.value = playerHealthSystem.CurrentHealth / playerHealthSystem.MaxHealth;
+    }
+
+    private void GameOver()
+    {
+        //gameOverUI.SetActive(true);
+    }
+
+    private void UpdateWaveUI()
+    {
+        //waveText.text;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
 }
