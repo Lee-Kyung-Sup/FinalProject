e87e56decb304946bf837af99780bb0bc947a7fe
Lee-Kyung-Sup(이경섭) <120997897+Lee-Kyung-Sup@users.noreply.github.com>
2024-03-12 17:57:01 +0900

    Feat_followingmonster

--- Assets/Scripts/Monster/Dino.cs
+++ Assets/Scripts/Monster/Dino.cs
@@ -2,17 +2,155 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dino : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class Dino : Monster
+{ 
+    public enum State
     {
-        
+        Idle,
+        Run,
+        Attack,
+        Jump
+    };
+
+    public State currentState = State.Idle;
+
+    public Transform[] wallCheck;
+    WaitForSeconds Delay500 = new WaitForSeconds(0.5f);
+
+    Vector2 capsuleColliderOffset;
+    Vector2 capsuleColliderJumpOffset;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        moveSpeed = 3f;
+        jumpPower = 10f;
+        currentHp = 6;
+        atkCoolTime = 3f;
+        atkCoolTimeCalc = atkCoolTime;
+
+        capsuleColliderOffset = capsuleCollider.offset;
+        capsuleColliderJumpOffset = new Vector2(capsuleColliderOffset.x, 1f);
+
+        StartCoroutine(FSM());
     }
 
-    // Update is called once per frame
+    IEnumerator FSM()
+    {
+        while(true)
+        {
+            yield return StartCoroutine(currentState.ToString());
+        }
+    }
+
+    IEnumerator Idle()
+    {
+        capsuleCollider.offset = capsuleColliderOffset;
+        yield return Delay500;
+        currentState = State.Run;
+    }
+
+    IEnumerator Run()
+    {
+        yield return null;
+        float runTime = Random.Range(2f, 4f);
+        while(runTime >= 0f)
+        {
+            runTime -= Time.deltaTime;
+            if (!isHit)
+            {
+                rb.velocity = new Vector2(-transform.localScale.x * moveSpeed, rb.velocity.y);
+
+
+                if (!Physics2D.OverlapCircle(wallCheck[0].position, 0.35f, layerMask[1]) &&  //벽체크 0번 플랫폼이없고
+                     Physics2D.OverlapCircle(wallCheck[1].position, 0.35f, layerMask[0]) /*&&  //1번이 플랫폼이면 몬스터 점프
+                    !Physics2D.Raycast(transform.position, -transform.localScale.x * transform.right, 1f, layerMask)*/)  //플랫폼과 너무 가까우면 올라가기 힘들기 때문에 넣음
+                {
+
+                    rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+                    Debug.Log(rb.velocity);
+                }
+
+                if ((Physics2D.OverlapCircle(wallCheck[0].position, 0.35f, layerMask[1]) &&
+                     Physics2D.OverlapCircle(wallCheck[1].position, 0.35f, layerMask[1])) &&
+                    (Physics2D.OverlapCircle(wallCheck[0].position, 0.35f, layerMask[0]) &&
+                     Physics2D.OverlapCircle(wallCheck[1].position, 0.35f, layerMask[0])))
+                {
+                    Debug.Log("t1");
+                    MonsterFlip();
+                }
+
+                if ((!Physics2D.OverlapCircle(wallCheck[2].position, 0.35f, layerMask[1]) &&
+                    !Physics2D.OverlapCircle(wallCheck[3].position, 0.35f, layerMask[1]) &&
+                    !Physics2D.OverlapCircle(wallCheck[4].position, 0.35f, layerMask[1])) &&
+                    (!Physics2D.OverlapCircle(wallCheck[2].position, 0.35f, layerMask[0]) &&
+                    !Physics2D.OverlapCircle(wallCheck[3].position, 0.35f, layerMask[0]) &&
+                    !Physics2D.OverlapCircle(wallCheck[4].position, 0.35f, layerMask[0])))
+                {
+                    Debug.Log("t2");
+                    MonsterFlip();
+                }
+
+                if (canAtk && IsPlayerDir() && isGround)
+                {
+                     if (Vector2.Distance(transform.position, GameManager.instance.GetPlayerPosition()) < 5f)
+                     {
+                          currentState = State.Attack;
+                          break;
+                     }
+                }
+            }
+            yield return null;
+        }
+        if(currentState != State.Attack &&
+           currentState != State.Jump)
+        {
+            if(!IsPlayerDir())
+            {
+                MonsterFlip();
+            }
+        }
+    } 
+
+    IEnumerator Attack()
+    {
+        yield return null;
+        if(!isHit && isGround)
+        {
+            capsuleCollider.offset = capsuleColliderJumpOffset;
+            canAtk = false;
+            rb.velocity = new Vector2(-transform.localScale.x * 10f, jumpPower/ 1.25f);
+            MyAnimSetTrigger("Attack");
+
+            yield return Delay500;
+            currentState = State.Idle;
+        }
+        else
+        {
+            currentState = State.Run;
+        }
+
+    }
+    IEnumerator Jump()
+    {
+        yield return null;
+        capsuleCollider.offset = capsuleColliderJumpOffset;
+
+        rb.velocity = new Vector2(-transform.localScale.x * 6f, jumpPower);
+        MyAnimSetTrigger("Attack");
+
+        yield return Delay500;
+        currentState = State.Idle;
+    }
+
     void Update()
     {
-        
+        GroundCheck();
+        if(!isHit && isGround && !IsPlayingAnim("Run"))
+        {
+            capsuleCollider.offset = capsuleColliderOffset;
+            MyAnimSetTrigger("Idle");
+        }
     }
 }
+
