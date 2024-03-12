91e46bc528db73635a1769daecc94e5bbc3769de
wrwrq <154485017+wrwrq@users.noreply.github.com>
2024-03-12 13:07:14 +0900

    Fix - St10

--- Assets/Scripts/Map/DestroyTile.cs
+++ Assets/Scripts/Map/DestroyTile.cs
@@ -6,13 +6,21 @@ using UnityEngine.Tilemaps;
 public class DestroyTile : MonoBehaviour
 {
     Tilemap tile;
+    MapEventChecker checker;
     private void Start()
     {
         tile = GetComponent<Tilemap>();
+        checker = MapMaker.Instance.mapEventCheker;
+        if (checker.isBroken.ContainsKey(gameObject))
+        {
+            Destroy(gameObject);
+            return;
+        }
     }
     public void MakeDot(Vector3 pos)
     {
         Vector3Int cellPosition = tile.WorldToCell(pos);
         tile.SetTile(cellPosition, null);
+        checker.isBroken.Add(gameObject,true);
     }
 }
--- Assets/Scripts/Map/MapEventChecker.cs
+++ Assets/Scripts/Map/MapEventChecker.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MapEventChecker : MonoBehaviour
 {
     public Dictionary<GameObject, bool> isClear = new Dictionary<GameObject, bool>();
+    public Dictionary<GameObject, bool> isBroken = new Dictionary<GameObject, bool>();
 }
