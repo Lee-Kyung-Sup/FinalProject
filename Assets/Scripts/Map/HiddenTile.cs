0f7c6c09e83578bf5865f15a5082898c6dc3dbdf
wrwrq <154485017+wrwrq@users.noreply.github.com>
2024-03-13 15:54:45 +0900

    Feat -st16

--- Assets/Scripts/Map/HiddenTile.cs
+++ Assets/Scripts/Map/HiddenTile.cs
@@ -17,15 +17,7 @@ public class HiddenTile : MonoBehaviour
     {
         if (pLayer.value == (pLayer.value | (1 << collision.gameObject.layer)))
         {
-            Debug.Log("a");
             tile.color = new Color(tile.color.r, tile.color.g, tile.color.b, 0.4f);
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (pLayer.value == (pLayer.value | (1 << collision.gameObject.layer)))
-        {
-            tile.color = new Color(tile.color.r, tile.color.g, tile.color.b, 1);
         }
     }
 }
