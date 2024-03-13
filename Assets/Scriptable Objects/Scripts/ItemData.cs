270ba31aed6f30df711995a45896d799a5f3f03d
todreami <140941478+todreami@users.noreply.github.com>
2024-03-13 17:45:10 +0900

    test
    test

--- Assets/Scriptable Objects/Scripts/ItemData.cs
+++ Assets/Scriptable Objects/Scripts/ItemData.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemData : MonoBehaviour
+public enum ItemType
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Resource,
+    Equipable,
+    Consumable
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public enum ConsumableType
+{
+    Hunger,
+    Health
+}
+
+[System.Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string displayName;
+    public string description;
+    public ItemType type;
+    public Sprite icon;
+    public GameObject dropPrefab;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmount;
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
+
 }
