fa1b3691193dc7ce658f27fab4c0f6e87a9280f8
wjj329 <wjj2187@naver.com>
2024-03-13 17:32:18 +0900

    feat. 플레이어 원거리 Hit 이펙트 추가

--- Assets/Scripts/Player/PlayerBulletController.cs
+++ Assets/Scripts/Player/PlayerBulletController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerBulletController : MonoBehaviour
 {
+    [SerializeField] private GameObject RangeHitEffect; // 히트 효과 프리팹
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +22,13 @@ public class PlayerBulletController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Platform"))
         {
             Debug.Log("벽에 충돌!");
+            Instantiate(RangeHitEffect, transform.position, Quaternion.identity); // 히트 효과 생성
             Destroy(gameObject);
         }
         else if (collision.gameObject.CompareTag("Monster"))
         {
             Debug.Log("몬스터에게 충돌!");
+            Instantiate(RangeHitEffect, transform.position, Quaternion.identity); // 히트 효과 생성
             // collision.SendMessage("Demaged", 10); // Demaged 함수 호출, 원거리 공격력(10, 임시)만큼 피해  TODO
             Destroy(gameObject);
         }
