df3458374f5d6c677814568d51750b89cc77c3d4
todreami <140941478+todreami@users.noreply.github.com>
2024-03-22 15:51:30 +0900

    fix/inventory/jaehyun/change inventory
    scriptableobject로 만든 걸 지우고 리스트로 만들었습니다. 아이템은 인벤토리에 수납되고, 저장되지만 아직 아이템 기능을 추가하지 못했습니다.

---
+++ Assets/1. Scripts/Item/Slot.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Slot : MonoBehaviour
+{
+    public Item slotsItem;
+
+    Sprite defaultSprite;
+    TMP_Text amountText;
+
+    public void CustomStart()
+    {
+        defaultSprite = GetComponent<Image>().sprite;
+        amountText = transform.GetChild(0).GetComponent<TMP_Text>();
+    }
+
+    public void DropItem()
+    {
+        if (slotsItem)
+        {
+            slotsItem.transform.parent = null;
+            slotsItem.gameObject.SetActive(true);
+            slotsItem.transform.position = Vector3.zero;
+        }
+    }
+
+    public void CheckForItem()
+    {
+        //if (slotsItem == null)
+        //    return;
+
+        if (transform.childCount > 1)
+        {
+            slotsItem = transform.GetChild(1).GetComponent<Item>();
+            GetComponent<Image>().sprite = slotsItem.itemSprite;
+            if (slotsItem.amountInStack > 1)
+            {
+                amountText.text = slotsItem.amountInStack.ToString();
+            }
+
+        }
+        else
+        {
+            slotsItem = null;
+            GetComponent<Image>().sprite = defaultSprite;
+            amountText.text = "";
+        }
+    }
+
+
+}
