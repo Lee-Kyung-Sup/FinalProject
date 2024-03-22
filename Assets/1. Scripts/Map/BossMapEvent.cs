348de7b9093b07e89cbcdd42620b55b8d7a8c092
wrwrq <154485017+wrwrq@users.noreply.github.com>
2024-03-22 10:54:15 +0900

    Feat - bossMapEvent

---
+++ Assets/1. Scripts/Map/BossMapEvent.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMapEvent : MapEvent
+{
+    Action BossMapEventF;
+    protected override IEnumerator Eventing()
+    {
+        for (int i = 0; i < mapPhase.Length; i++)
+        {
+            for (int p = 0; p < mapPhase[i].summonMonster.Length; p++)
+            {
+                CallMonster(mapPhase[i].summonMonster[p], mapPhase[i].summonPos[p]);
+            }
+            if (i == 0)
+            {
+                //TODO 벽 및 몬스터 생성 연출 종료후
+                playerAction.enabled = true;
+                BossMapEventF = MapMaker.Instance.BossMapEvents.GiveBossEvent();
+                BossMapEventF?.Invoke();
+            }
+            yield return isAllDieMonster;
+        }
+        ClearEvent();
+    }
+}
+public class BossMapEventList
+{
+    public Action GiveBossEvent()
+    {
+        switch (MapMaker.Instance.CurChapterId)
+        {
+            case 0:
+                return ChBoss1;
+            case 1:
+                return ChBoss2;
+            case 2:
+                return ChBoss3;
+            default:
+                return null;
+        }
+    }
+    void ChBoss1()
+    {
+
+    }
+    void ChBoss2()
+    {
+        OneWayMovePlatform.isStart = true;
+    }
+    void ChBoss3()
+    {
+
+    }
+}
