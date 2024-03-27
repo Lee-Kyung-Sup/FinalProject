fb01d4e514aabb6227d1e1984223642a3c6de22e
todreami <140941478+todreami@users.noreply.github.com>
2024-03-27 16:42:47 +0900

    feat. audiomanager 구축중

--- Assets/1. Scripts/Common/Sound.cs
+++ Assets/1. Scripts/Common/Sound.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sound : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
 }
