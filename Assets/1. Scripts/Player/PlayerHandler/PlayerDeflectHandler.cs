3f62ca79ef13a94c6034a83b24d62a0b6236c663
wjj329 <wjj2187@naver.com>
2024-03-28 18:32:52 +0900

    fix. 충돌 검사 조건문 레이어 비트 연산으로 변경

--- Assets/1. Scripts/Player/PlayerHandler/PlayerDeflectHandler.cs
+++ Assets/1. Scripts/Player/PlayerHandler/PlayerDeflectHandler.cs
@@ -9,7 +9,7 @@ public class PlayerDeflectHandler : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("EnemyBullet") || collision.gameObject.CompareTag("Projectile"))
+        if (((1 << collision.gameObject.layer) & ((1 << 11) | (1 << 12))) != 0) // 11. Projectile  12. EnemyBullet
         {
             Rigidbody2D bulletRb = collision.GetComponent<Rigidbody2D>();
 
@@ -19,7 +19,8 @@ public class PlayerDeflectHandler : MonoBehaviour
                 Vector2 deflectDirection = new Vector2(-bulletRb.velocity.x * deflectPower, -bulletRb.velocity.y * deflectPower);
                 bulletRb.velocity = deflectDirection;
 
-                collision.gameObject.layer = LayerMask.NameToLayer("PlayerBullet");
+                //collision.gameObject.layer = LayerMask.NameToLayer("PlayerBullet");
+                collision.gameObject.layer = 20;
                 // 몬스터가 PlayerBullet에 닿을 때 데미지를 주도록 데미지 로직 수정 TODO
             }
         }
--- Assets/1. Scripts/Player/PlayerHandler/PlayerRangeAttackHandler.cs
+++ Assets/1. Scripts/Player/PlayerHandler/PlayerRangeAttackHandler.cs
@@ -23,7 +23,7 @@ public class PlayerRangeAttackHandler : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Platform"))
+        if (((1 << collision.gameObject.layer) & ((1 << 8) | (1 << 9))) != 0) // 8: 그라운드 , 9: 플랫폼
         {
             if (!isChargeShot) // 일반 샷인 경우에만 벽이나 플랫폼에 부딪혔을 때 사라짐
             {
@@ -33,7 +33,7 @@ public class PlayerRangeAttackHandler : MonoBehaviour
 
         }
 
-        else if (collision.gameObject.CompareTag("Monster"))
+        else if (((1 << collision.gameObject.layer) & (1 << 7)) != 0) // 7: 몬스터
         {
             Instantiate(RangeHitEffect, transform.position, Quaternion.identity); // 히트 효과 생성
             // collision.SendMessage("Demaged", 1); // Demaged 함수 호출, 원거리 공격력(1, 임시)만큼 피해  TODO
