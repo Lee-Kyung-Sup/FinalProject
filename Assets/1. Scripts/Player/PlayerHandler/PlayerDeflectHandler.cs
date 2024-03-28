01b807cd5a727c4ebe5a041f62a2784b536b97a9
wjj329 <wjj2187@naver.com>
2024-03-28 15:59:52 +0900

    fix. 반사시 투사체 레이어 변경

--- Assets/1. Scripts/Player/PlayerHandler/PlayerDeflectHandler.cs
+++ Assets/1. Scripts/Player/PlayerHandler/PlayerDeflectHandler.cs
@@ -18,6 +18,9 @@ public class PlayerDeflectHandler : MonoBehaviour
                 Instantiate(deflectEffect, collision.transform.position, Quaternion.identity);
                 Vector2 deflectDirection = new Vector2(-bulletRb.velocity.x * deflectPower, -bulletRb.velocity.y * deflectPower);
                 bulletRb.velocity = deflectDirection;
+
+                collision.gameObject.layer = LayerMask.NameToLayer("PlayerBullet");
+                // 몬스터가 PlayerBullet에 닿을 때 데미지를 주도록 데미지 로직 수정 TODO
             }
         }
     }
