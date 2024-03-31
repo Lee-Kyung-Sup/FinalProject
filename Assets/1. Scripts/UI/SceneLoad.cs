ee24b92ab6814e922d6c9463235f3c8aa999f506
todreami <140941478+todreami@users.noreply.github.com>
2024-04-01 08:54:10 +0900

    feat/jaehyun/ETC/로딩화면구현

--- Assets/1. Scripts/UI/SceneLoad.cs
+++ Assets/1. Scripts/UI/SceneLoad.cs
@@ -9,6 +9,7 @@ public class SceneLoad : MonoBehaviour
 {
     public Slider progressbar;
     public TMP_Text loadtext;
+
     private void Start()
     {
         StartCoroutine(LoadScene());
@@ -19,33 +20,29 @@ public class SceneLoad : MonoBehaviour
         yield return null;
         AsyncOperation operation = SceneManager.LoadSceneAsync("2. GameScenes");
         operation.allowSceneActivation = false;
-
+ 
         while (!operation.isDone)
         {
-            yield return null;
-            if (progressbar.value < 0.9f)
-            {
-                progressbar.value = Mathf.MoveTowards(progressbar.value, 0.9f, Time.deltaTime);
-            }
-            else if(operation.progress >=0.9f)
-            {
-                progressbar.value = Mathf.MoveTowards(progressbar.value, 1f, Time.deltaTime);
-            }
+                yield return null;
+                if (progressbar.value < 0.9f)
+                {
+                    progressbar.value = Mathf.MoveTowards(progressbar.value, 0.9f, Time.deltaTime);
+                }
+                else if (progressbar.value >= 0.9f)
+                {
+                    progressbar.value = Mathf.MoveTowards(progressbar.value, 1f, Time.deltaTime);
+                }
 
-            if(progressbar.value >= 1f)
-            {
-                loadtext.text = "PressSpaceBar";
-            }
 
-            if (Input.GetKeyDown(KeyCode.Space) && progressbar.value > 1f && operation.progress >=0.9f)
-            {
-                operation.allowSceneActivation = true;
-            }
+                if(progressbar.value >= 1f)
+                {
+                    loadtext.text = "Press SpaceBar";
+                }
 
+                if (Input.GetKeyDown(KeyCode.Space) && progressbar.value >= 1f && operation.progress >= 0.9f)
+                {
+                    operation.allowSceneActivation = true;
+                }
         }
-
-
-
-
     }
 }
