85f873bca6a9ed0701adea1640621126d2e77031
wrwrq <154485017+wrwrq@users.noreply.github.com>
2024-04-01 11:01:43 +0900

    Fix - 플레이어 감지

--- Assets/1. Scripts/Monster/Enemy/Enemy.cs
+++ Assets/1. Scripts/Monster/Enemy/Enemy.cs
@@ -20,7 +20,7 @@ public class Enemy : Entity,IDamageable
     {
         base.Awake();
         stateMachine = new EnemyStateMachine();
-        pLayer = LayerMask.GetMask("Player");
+        pLayer = LayerMask.GetMask("Player", "PlayerInvincible");
     }
     protected override void Update()
     {
